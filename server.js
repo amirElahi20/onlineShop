fb5a05ebac4fd454adb192739ad373631889bbd9
amirElahi20 <amiirelahii80@gmail.com>
2021-05-15 16:56:17 +0430

    changed the server document in vue project to fix router history

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const express = require('express')
+var express = require('express')
+var history = require('connect-history-api-fallback')
+var path = require('path')
+var serveStatic = require('serve-static')
 
-const serverStatic = require('serve-static')
 
-const path = require('path')
+var app = express()
 
-const app = express()
+// Use a fallback for non-root routes (required for Vue router)
+//   NOTE: History fallback must be "used" before the static serving middleware!
+app.use(history({
+    // OPTIONAL: Includes more verbose logging
+    verbose: true
+}))
 
-app.use('/', serverStatic(path.join(__dirname, '/dist')))
+// Serve static assets from the build files (images, etc)
+app.use(serveStatic(path.join(__dirname, '/dist')))
 
-const port = process.env.PORT || 8080
+var port = process.env.PORT || 8080
 
-app.listen(port)
+app.listen(port, () => {
+    console.log('Server started at http://localhost:5000')
+})
